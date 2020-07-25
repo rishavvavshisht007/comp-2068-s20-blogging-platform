3b7eba697355d0e1a254ecb2bf910c32a8b2e56c
Rishav Vashisht <rishavvashisht845@gmail.com>
2020-07-25 13:36:21 -0400

    commiting

--- app.js
+++ app.js
@@ -46,6 +46,8 @@ res.locals.pageTitle = "untitled";
 
 //passing along flash message
 res.locals.flash = req.flash();
+res.locals.formData = req.session.formData || {};
+res.session.formData = {};
 console.log(res.locals.flash);
 
 next();
--- controllers/BlogsController.js
+++ controllers/BlogsController.js
@@ -1,16 +1,32 @@
 const viewPath = ('blogs');
 const Blog = require('../models/blog');
 
-exports.index = (req, res) => {
-    res.send("hope all are here and safe");
+exports.index = async (req, res) => {
+    try{
+    const blogs = await Blog.find();
+    res.render(`${viewPath}/index`, {
+        pageTitle:'Archive',
+        blogs: blogs
+    });
+}   catch(error){
+    res.flash('danger', `Stop! There was an error, find it:${error}`);
+    res.redirect('/');
+}
 };
 
 exports.show = async (req, res) => {
+    try{
 const blog = await Blog.findById(req.params.id);
 res.render(`${viewPath}/show`,{
 pageTitle: blog.title,
 blog:blog
 });
+    }catch(error){
+        res.flash('danger', `Stop! There was an displaying this 
+        blog:${error}`);
+        res.redirect('/');
+    }
+    
 };
 
 exports.new = (req,res) => {
@@ -27,15 +43,26 @@ exports.create = async(req, res) => {
        req.flash('success', 'Blog created succesfully');
        res.redirect(`/blogs/${blog.id}`);
    } 
-   catch(err){
+   catch(error){
        req.flash('danger', `there was an error creating this blog:${error}`);
-       res.redirect('/new');
+       req.session.formData = req.body;
+       res.redirect('blogs/new');
    }
 
 };
 
-exports.edit = (req, res) => {
-    res.send("Whats up");
+exports.edit = async (req, res) => {
+    try{
+        const blog = await Blog.findById(req.params.id);
+        res.render(`${viewPath}/edit`,{
+        pageTitle: blog.title,
+        formData: blog
+        });
+    } catch{
+        res.flash('danger', `Stop! There was an accesing this
+        blog:${error}`);
+        res.redirect('/');
+    }
 };
 
 exports.update = (req, res) => {
