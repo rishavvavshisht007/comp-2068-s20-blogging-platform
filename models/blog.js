d806530100b16769f1de59e7ceb148851a9732e1
Rishav Vashisht <rishavvashisht845@gmail.com>
2020-07-29 14:10:33 -0400

    pushing code

--- models/blog.js
+++ models/blog.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const BlogSchema = new mongoose.Schema({
     title:{
         type:String,
-        required:true //this must exist
+        required:true
     },
     content:{
         type:String,
